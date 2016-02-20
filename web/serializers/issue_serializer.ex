258fa133333f086d4660e7b019d2749acc0df663
Romain Champourlier <pro@rchampourlier.com>
2016-02-20 15:20:39 +0100

    [API] Changes to issue serialization
    - estimate changed to hours
    - added developer and reviewer fields (custom to JobTeaser's JIRA for now)

--- web/serializers/issue_serializer.ex
+++ web/serializers/issue_serializer.ex
@@ -1,7 +1,7 @@
 defmodule PlanningToolApi.IssueSerializer do
   use Remodel
 
-  attributes [:key, :summary, :description, :estimate]
+  attributes [:key, :summary, :description, :estimate, :developer, :reviewer]
 
   def key(record) do
     record["key"]
@@ -15,7 +15,19 @@ defmodule PlanningToolApi.IssueSerializer do
     record["fields"]["description"]
   end
 
-  def estimate(record) do
-    record["fields"]["timetracking"]["remainingEstimateSeconds"]
+  # Returns remaining estimate in hours
+  def estimate(%{"fields" => %{"timetracking" => %{"remainingEstimateSeconds" => value}}}) do
+    value / 3600
+  end
+  def estimate(_) do
+    0
+  end
+
+  def developer(record) do
+    record["fields"]["customfield_10600"]["key"]
+  end
+
+  def reviewer(record) do
+    record["fields"]["customfield_10601"]["key"]
   end
 end
